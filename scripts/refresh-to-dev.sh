eb712ca5a1654a3132b1edd9dbeb52fd5be90ad4
turnipxenon <turnipxenon@gmail.com>
2024-04-26 20:24:54 -0600

    Add refresh-to-dev.sh

---
+++ scripts/refresh-to-dev.sh
@@ -0,0 +1,4 @@
+﻿# refresh main and go back to the current branch with the same HEAD as main
+# reference: https://stackoverflow.com/q/1417957/17836168
+turnip_last_branch=$(git branch | awk '/\*/ { print $2; }')
+git checkout main && git pull origin main && git branch -d "$turnip_last_branch" && git checkout -b "$turnip_last_branch"
